195aeaa3d1381fe05f0907e7b829fea784f3feca
Kuboserd <Kuboserd@gmail.com>
2023-05-23 15:33:07 +0200

    Menu Scene updated , now shows title, background and several test buttons

--- src/main/java/gui/GUI.java
+++ src/main/java/gui/GUI.java
@@ -1,24 +1,48 @@
- package gui;
+package gui;
 
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
-import javafx.scene.layout.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import java.io.FileNotFoundException;
 
 public class GUI extends Application {
 
     @Override
-    public void start(Stage stage) throws FileNotFoundException {
-        StackPane root = new StackPane();
-
-        BackgroundImage backgroundImage = new BackgroundImage(new Image("files:resources/menu.png", 1920, 1080, false, true),
-                BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
-
-        Scene menu = new Scene(root, 600, 400);
-        root.setBackground(new Background(backgroundImage));
+    public void start(Stage stage) {
+        Group root = new Group();
+        Scene menu = new Scene(root);
         stage.setScene(menu);
+        Canvas canvas = new Canvas(1280, 720);
+        root.getChildren().add(canvas);
+        GraphicsContext context = canvas.getGraphicsContext2D();
+        Image mainMenu = new Image("menu.jpg", 1280, 720, false, false);
+        context.drawImage(mainMenu, 0, 0);
+
+        Text text = new Text();
+        text.setFont(new Font(45));
+        text.setFill(Color.WHITE);
+        text.setX(530);
+        text.setY(100);
+        text.setText("POKEMON");
+        root.getChildren().add(text);
+
+        GridPane gridPane = new GridPane();
+        gridPane.setVgap(20);
+        gridPane.setHgap(265);
+        gridPane.add(new Button("TEST1"), 2,10);
+        gridPane.add(new Button("TEST2"), 2,11);
+        gridPane.add(new Button("TEST3"), 2,12);
+
+        root.getChildren().add(gridPane);
+
         stage.show();
     }
 
